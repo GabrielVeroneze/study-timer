2723f722bb819a01ac701c54a8e119abceafc3de
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-01 15:52:56 -0300

    :sparkles: feat: Adiciona página inicial

---
+++ src/pages/Inicio/index.tsx
@@ -0,0 +1,13 @@
+import Formulario from "components/Formulario"
+import Lista from "components/Lista"
+
+const Inicio = () => {
+    return (
+        <>
+            <Formulario />
+            <Lista />
+        </>
+    )
+}
+
+export default Inicio
