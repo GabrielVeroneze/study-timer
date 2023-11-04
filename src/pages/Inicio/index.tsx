9c065aeb190ebe525dee1a0ed7fe3d032b021386
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-04 17:55:14 -0300

    :sparkles: feat: Adiciona e tipa o estado de tarefas

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,13 +1,30 @@
+import { useState } from "react"
+import { ITarefa } from "interfaces/ITarefa"
 import Formulario from "components/Formulario"
 import Lista from "components/Lista"
 import Cronometro from "components/Cronometro"
 import styles from './Inicio.module.scss'
 
 const Inicio = () => {
+    const [tarefas, setTarefas] = useState<ITarefa[]>([
+        {
+            nome: 'React',
+            tempo: '02:00:00',
+        },
+        {
+            nome: 'JavaScript',
+            tempo: '01:00:00',
+        },
+        {
+            nome: 'TypeScript',
+            tempo: '03:00:00'
+        }
+    ])
+
     return (
         <main className={styles.inicio}>
             <Formulario />
-            <Lista />
+            <Lista tarefas={tarefas} />
             <Cronometro />
         </main>
     )
