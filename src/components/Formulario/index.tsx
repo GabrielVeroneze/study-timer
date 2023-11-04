af2221c5fd459fdbb8a590b63a8164e95cfc0ef3
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-03 23:33:00 -0300

    :sparkles: feat: Controla o formulário
    Adiciona o estado de "tarefa" que é um objeto com as propriedade de "nome" e "tempo". Utiliza ele para controlar a entrada de dados no formulário. Também adiciona evento de "submit" e uma função para manipular esse evento.

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,15 +1,38 @@
+import { useState } from 'react'
 import CampoDeEntrada from 'components/CampoDeEntrada'
 import Botao from 'components/Botao'
 import styles from './Formulario.module.scss'
 
+interface ITarefa {
+    nome: string
+    tempo: string
+}
+
 const Formulario = () => {
+    const [tarefa, setTarefa] = useState<ITarefa>({
+        nome: '',
+        tempo: '00:00'
+    })    
+
+    const adicionarTarefa = (evento: React.FormEvent<HTMLFormElement>) => {
+        evento.preventDefault()
+        console.log(tarefa);
+    }
+
     return (
-        <form className={styles.formulario}>
+        <form className={styles.formulario} onSubmit={adicionarTarefa}>
             <CampoDeEntrada label='Adicione um novo estudo'>
                 <input
                     type='text'
                     placeholder='O que você quer estudar'
                     required
+                    value={tarefa.nome}
+                    onChange={evento => 
+                        setTarefa({
+                            ...tarefa,
+                            nome: evento.target.value
+                        })
+                    }
                 />
             </CampoDeEntrada>
             <CampoDeEntrada label='Tempo'>
@@ -19,6 +42,13 @@ const Formulario = () => {
                     min='00:00:00'
                     max='01:30:00'
                     required
+                    value={tarefa.tempo}
+                    onChange={evento => 
+                        setTarefa({
+                            ...tarefa,
+                            tempo: evento.target.value
+                        })
+                    }
                 />
             </CampoDeEntrada>
             <Botao>Adicionar</Botao>
