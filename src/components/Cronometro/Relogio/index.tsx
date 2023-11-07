a8bf0e93181e7e06150c814a96cf3af678525451
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-07 19:05:11 -0300

    :sparkles: feat: Mostra o tempo da tarefa no relógio
    Calcula o tempo em segundos recebido e em duas constantes, uma contém o resultado do calculo em minutos e a outra com os segundos restantes. Depois, converte as constantes de minutos e segundos em strings preenchendo elas com "0" e é desestruturada em duas variáveis de dezena e unidade.

--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -1,13 +1,24 @@
 import styles from './Relogio.module.scss'
 
-const Relogio = () => {
+interface RelogioProps {
+    tempo: number | undefined
+}
+
+const Relogio = ({ tempo = 0 }: RelogioProps) => {
+    
+    const minutos = Math.floor(tempo / 60)
+    const segundos = tempo % 60
+
+    const [minutoDezena, minutoUnidade] = String(minutos).padStart(2, '0')
+    const [segundoDezena, segundoUnidade] = String(segundos).padStart(2, '0')
+
     return (
         <>
-            <span className={styles.numero}>0</span>
-            <span className={styles.numero}>0</span>
+            <span className={styles.numero}>{minutoDezena}</span>
+            <span className={styles.numero}>{minutoUnidade}</span>
             <span className={styles.divisao}>:</span>
-            <span className={styles.numero}>0</span>
-            <span className={styles.numero}>0</span>
+            <span className={styles.numero}>{segundoDezena}</span>
+            <span className={styles.numero}>{segundoUnidade}</span>
         </>
     )
 }
