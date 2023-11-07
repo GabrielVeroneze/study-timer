6ffe4bc7ec1eff2b8afc681a1a080d0a733e1559
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-06 23:46:11 -0300

    :sparkles: feat: Cria estado de tempo
    Cria estado de tempo e adiciona como valor o tempo da tarefa selecionada convertida em segundos.

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,8 +1,21 @@
+import { useState } from 'react'
+import { ITarefa } from 'interfaces/ITarefa'
+import { tempoParaSegundos } from 'common/utils/time'
 import Botao from 'components/Botao'
 import Relogio from './Relogio'
 import styles from './Cronometro.module.scss'
 
-const Cronometro = () => {
+interface CronometroProps {
+    selecionado: ITarefa | undefined
+}
+
+const Cronometro = ({ selecionado }: CronometroProps) => {
+    const [tempo, setTempo] = useState<number>()
+
+    if (selecionado?.tempo) {
+        setTempo(tempoParaSegundos(selecionado.tempo))
+    }
+
     return (
         <div className={styles.cronometro}>
             <p className={styles.titulo}>
